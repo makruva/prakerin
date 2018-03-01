7c5a778abb75191af24272e6426327e083c39f41
AndroidUnik <35129313+hasifpriyambudi@users.noreply.github.com>
2018-03-01 13:37:31 +0700

    Add files via upload

---
+++ gastrik/model/M_penjualan.php
@@ -0,0 +1,39 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class M_penjualan extends CI_Model {
+	var $tbl = 'penjualan';
+
+	public function insert($data){
+		$res = $this->db->insert($this->tbl, $data);
+		return $res;
+	}
+
+	public function lists($id_user){
+		//$this->db->where('id_user', $id_user);
+		//$data = $this->db->get('penjualan Natural JOIN customer ON penjualan.id_pelanggan = customer.id_cust');
+		//return $data->result();
+		$this->db->select('*');
+		$this->db->from('penjualan');
+		$this->db->join('customer', 'penjualan.id_pelanggan = customer.id_cust');
+		$result = $this->db->get();
+			if ($result->num_rows() > 0)
+			{
+				return $result->row();
+			}else{
+				return FALSE;
+			}
+		}
+
+
+	public function update($id_penjualan, $data){
+		$this->db->where('id_penjualan', $id_penjualan);
+			$res = $this->db->update($this->tbl,$data);
+			return $res;
+		}
+
+	public function delete($id_penjualan){
+		$this->db->where('id_penjualan',$id_penjualan);
+		$res = $this->db->delete($this->tbl);
+		}
+}
